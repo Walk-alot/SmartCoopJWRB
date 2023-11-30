7955c3e0af1503aeadcdadf80590c9b4ad1b97a3
John Walker <johnwalkersmhs@gmail.com>
2023-11-30 13:29:26 -0600

    weather

--- CoopFresh/JS/main.js
+++ CoopFresh/JS/main.js
@@ -11,6 +11,7 @@ $('#btnToggleLogin').click(function(){
 
 $('#btnNavLogin').click(function(){
     $('#divLogin').slideToggle()
+    $('#divRegister').hide()
 })
 
 $('#btnNavLogout').click(function(){
@@ -24,6 +25,7 @@ $('#btnNavLogout').click(function(){
     $('#ACDash').hide()
     $('#HomeDash').hide()
     $('#FoodWaterDash').hide()
+    $('#weatherInfo').hide()
 })
 
 $('#btnHomeDash').click(function(){
@@ -66,6 +68,7 @@ $('#btnFoodWaterDash').click(function(){
     $('#HomeDash').hide()
 })
 
+
 //Login Card
 $('#btnLogin').click(function () {
     let strUsername = $('#txtLoginEmail').val();
@@ -96,6 +99,7 @@ $('#btnLogin').click(function () {
         $('#btnMyAccountToggle').show()
         $('#btnNavLogout').show()
         $('#btnNavLogin').hide()
+        $('#weatherInfo').show()
     }
 })
 
@@ -180,5 +184,61 @@ $('#btnRegister').click(function(){
         $('#btnMyAccountToggle').show()
         $('#btnNavLogout').show()
         $('#btnNavLogin').hide()
+        $('#weatherInfo').show()
     }           
 })
+
+// Egg Counter
+
+// Weather Display
+
+async function getWeather() {
+    const apiKey = '434cfb936758e07ed174697f92432b11';
+    const city = 'cookeville';
+
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        displayWeather(data);
+    } catch (error) {
+        console.error('Error fetching weather data:', error);
+        displayWeatherError();
+    }
+}
+
+function displayWeather(weatherData) {
+    const city = weatherData.name;
+    const temperature = weatherData.main.temp;
+    const weatherDescription = weatherData.weather[0].description;
+    const timestamp = weatherData.dt;
+    const time = new Date(timestamp * 1000).toLocaleTimeString();
+
+    const weatherInfo = `
+        <h2>Weather in ${city}</h2>
+        <p>Description: <label class="text-uppercase">${weatherDescription}</label></p>
+        <p>Temperature: ${temperature} °F</p>
+        <p>Time: ${time}</p>
+    `;
+
+    document.getElementById('weatherInfo').innerHTML = weatherInfo;
+
+    console.log('Displaying weather...');
+}
+
+function displayWeatherError() {
+    document.getElementById('weatherInfo').innerHTML = '<h2>Failed to fetch weather data.</h2>';
+}
+
+// Fetch weather data initially
+getWeather();
+
+// Update weather data every 10 minutes (600,000 milliseconds)
+setInterval(getWeather, 10000)
+    
+// Fetch weather data when the page loads
+document.addEventListener('DOMContentLoaded', getWeather);
+
+console.log('Weather last updated at:', new Date());
+// End Weather Display
